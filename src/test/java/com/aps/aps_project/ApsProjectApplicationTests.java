3cb5158f98cc18288c330eed3cd052f237fb5dab
LehTaiyou <98107826+LehTaiyou@users.noreply.github.com>
2024-09-06 21:14:42 -0300

    Add files via upload

---
+++ src/test/java/com/aps/aps_project/ApsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.aps_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
