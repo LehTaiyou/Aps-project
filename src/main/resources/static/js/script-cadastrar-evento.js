a5bf07f9b76eff73113704d99f81fb737cd547a5
jkemuel <155025572+jkemuel@users.noreply.github.com>
2024-09-18 23:40:38 -0300

    Tela de cadastrar eventos

---
+++ src/main/resources/static/js/script-cadastrar-evento.js
@@ -0,0 +1,54 @@
+document.getElementById('event-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do formulário
+
+    // Coleta os dados do formulário
+    var titulo = document.getElementById('titulo').value;
+    var descricao = document.getElementById('descricao').value;
+    var local = document.getElementById('local').value;
+    var data = document.getElementById('data').value;
+    var cargaHoraria = parseFloat(document.getElementById('carga-horaria').value);
+    var maxParticipantes = parseInt(document.getElementById('participantes').value);
+
+    // Verifica se a data está no formato dd/MM/yyyy usando uma expressão regular
+    var datePattern = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (!datePattern.test(data)) {
+        alert("Por favor, insira a data no formato DD/MM/YYYY.");
+        return;
+    }
+
+    // Converte a data de dd/MM/yyyy para yyyy-MM-dd
+    var dateParts = data.split("/");  // Divide a data em [dia, mes, ano]
+    var formattedDate = dateParts[2] + "-" + dateParts[1] + "-" + dateParts[0];  // Reorganiza para yyyy-MM-dd
+
+    // Cria um objeto com os dados
+    var eventoData = {
+        titulo: titulo,
+        descricao: descricao,
+        local: local,
+        data: formattedDate,  // Agora a data está no formato yyyy-MM-dd
+        cargaHoraria: cargaHoraria,
+        maxParticipantes: maxParticipantes
+    };
+
+    // Envia os dados via fetch
+    fetch('/eventos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(eventoData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao criar o evento');
+        }
+        return response.json(); // Pega o JSON da resposta
+    })
+    .then(result => {
+        console.log('ok')
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Erro ao enviar o formulário: ' + error.message);
+    });
+});
