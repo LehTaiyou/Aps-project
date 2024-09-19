5de07a1bcadcca1db74d2c686e0adb5e850e77a3
jkemuel <155025572+jkemuel@users.noreply.github.com>
2024-09-18 23:43:23 -0300

    Atualizado Evento.java

--- src/main/java/com/aps/aps_project/model/Evento.java
+++ src/main/java/com/aps/aps_project/model/Evento.java
@@ -1,5 +1,7 @@
 package com.aps.aps_project.model;
 
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,15 +14,22 @@ public class Evento {
     @Id
     private String id;
     private String titulo;
-    private String curso;
-    private int numHoras;
+    private String descricao;
+    private String local;
+    private Date data;
+    private float cargaHoraria;
+    private int maxParticipantes;
 
     public Evento() {
     }
 
-    public Evento(String titulo, String curso, int numHoras) {
+    public Evento(String id, String titulo, String descricao, String local, Date data, float cargaHoraria, int maxParticipantes) {
+        this.id = id;
         this.titulo = titulo;
-        this.curso = curso;
-        this.numHoras = numHoras;
+        this.descricao = descricao;
+        this.local = local;
+        this.data = data;
+        this.cargaHoraria = cargaHoraria;
+        this.maxParticipantes = maxParticipantes;
     }
 }
